8a76c627cc67eae82e03668ff66c9ff7cd97d64c
LiHui <hui.li@thoughtworks.com>
2021-12-02 16:23:12 +0800

    [lihui] feat: finish refresh list button function and change version to 0.0.8

--- src/main/java/net/lihui/app/plugin/thoughtworkscodereviewtools/ui/CodeReviewFeedbackPanel.java
+++ src/main/java/net/lihui/app/plugin/thoughtworkscodereviewtools/ui/CodeReviewFeedbackPanel.java
@@ -1,10 +1,19 @@
 package net.lihui.app.plugin.thoughtworkscodereviewtools.ui;
 
+import com.julienvey.trello.domain.Member;
+import net.lihui.app.plugin.thoughtworkscodereviewtools.client.TrelloClient;
+import net.lihui.app.plugin.thoughtworkscodereviewtools.idea.notification.Notifier;
 import net.lihui.app.plugin.thoughtworkscodereviewtools.idea.store.TrelloBoardMember;
 import net.lihui.app.plugin.thoughtworkscodereviewtools.idea.store.TrelloBoardMemberState;
+import net.lihui.app.plugin.thoughtworkscodereviewtools.idea.store.TrelloConfiguration;
+import net.lihui.app.plugin.thoughtworkscodereviewtools.idea.store.TrelloMemberProperties;
+import net.lihui.app.plugin.thoughtworkscodereviewtools.idea.store.TrelloState;
+import net.lihui.app.plugin.thoughtworkscodereviewtools.service.CodeReviewBoardService;
 import net.lihui.app.plugin.thoughtworkscodereviewtools.ui.dto.OwnerCheckboxDTO;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 
 import static net.lihui.app.plugin.thoughtworkscodereviewtools.mapper.MemberMapper.MEMBER_MAPPER;
@@ -28,6 +37,25 @@ public class CodeReviewFeedbackPanel {
         ownerListTable.setModel(tableModel);
         ownerListTable.setDefaultRenderer(OwnerCheckboxDTO.class, new TableCheckboxCellRenderer());
         ownerListTable.setDefaultEditor(OwnerCheckboxDTO.class, new TableCheckboxCellEditor());
+        refreshOwnerListButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                Notifier.notifyInfo(null, "hello");
+                fetchBoardMemberList();
+                trelloBoardMembers = TrelloBoardMemberState.getInstance().getState().getTrelloBoardMembers();
+                tableModel = new OwnerCheckboxTableModel(MEMBER_MAPPER.toDtoList(trelloBoardMembers));
+                ownerListTable.setModel(tableModel);
+            }
+        });
+    }
+
+    private void fetchBoardMemberList() {
+        TrelloConfiguration trelloConfiguration = TrelloState.getInstance().getState();
+        TrelloBoardMemberState boardMemberState = TrelloBoardMemberState.getInstance();
+
+        CodeReviewBoardService codeReviewBoardService = new CodeReviewBoardService(new TrelloClient(trelloConfiguration));
+        List<Member> trelloBoardMembers = codeReviewBoardService.getTrelloBoardMembers();
+        boardMemberState.setTrelloMemberProperties(new TrelloMemberProperties(MEMBER_MAPPER.toStateList(trelloBoardMembers)));
     }
 
     public JPanel getMainPanel() {
