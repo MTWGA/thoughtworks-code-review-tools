74c36b3019e1e7c759a349fc6305597f3f1e8467
Jie Peng <773411997@qq.com>
2021-12-03 11:16:02 +0800

    [pengjie] fix: fix json parse unknow field exception

--- src/main/java/net/lihui/app/plugin/thoughtworkscodereviewtools/entity/TrelloList.java
+++ src/main/java/net/lihui/app/plugin/thoughtworkscodereviewtools/entity/TrelloList.java
@@ -1,5 +1,6 @@
 package net.lihui.app.plugin.thoughtworkscodereviewtools.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TrelloList {
     private String id;
     private String name;
